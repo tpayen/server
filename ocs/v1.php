b04d4bff3fd06f59afade604db9fae460b5da126
Joas Schilling <coding@schilljs.com>
2022-07-04 19:02:16 +0200

    Also skip the login attempt from ocs base
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- ocs/v1.php
+++ ocs/v1.php
@@ -55,7 +55,7 @@ try {
 	// side effects in existing apps
 	OC_App::loadApps();
 
-	if (!\OC::$server->getUserSession()->isLoggedIn()) {
+	if (!\OC::$server->getUserSession()->isLoggedIn() && !\OC::$server->getRequest()->getHeader('X-Nextcloud-Federation')) {
 		OC::handleLogin(\OC::$server->getRequest());
 	}
 
