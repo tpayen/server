sleiner/nextcloud-server

Expose parts through webdav upload folder
Signed-off-by: Julius Härtl <jus@bitgrid.net>

apps/dav/lib/Upload/UploadFolder.php
@@ -24,7 +24,9 @@
  */
 namespace OCA\DAV\Upload;
 
+use OC\Files\ObjectStore\ObjectStoreStorage;
 use OCA\DAV\Connector\Sabre\Directory;
+use OCP\Files\ObjectStore\IObjectStoreMultiPartUpload;
 use OCP\Files\Storage\IStorage;
 use Sabre\DAV\Exception\Forbidden;
 use Sabre\DAV\ICollection;
@@ -68,6 +70,23 @@ class UploadFolder implements ICollection {
 
 		foreach ($tmpChildren as $child) {
 			$children[] = new UploadFile($child);
+		}
+
+		if ($this->storage->instanceOfStorage(ObjectStoreStorage::class)) {
+			/** @var ObjectStoreStorage $storage */
+			$objectStore = $this->storage->getObjectStore();
+			if ($objectStore instanceof IObjectStoreMultiPartUpload) {
+				$cache = \OC::$server->getMemCacheFactory()->createDistributed(ChunkingV2Plugin::OBJECT_UPLOAD_CACHE_KEY);
+				$uploadSession = $cache->get($this->getName());
+				if ($uploadSession) {
+					$uploadId = $uploadSession[ChunkingV2Plugin::OBJECT_UPLOAD_CHUNKTOKEN];
+					$id = $uploadSession[ChunkingV2Plugin::OBJECT_UPLOAD_TARGET_ID];
+					$parts = $objectStore->getMultipartUploads($this->storage->getURN($id), $uploadId);
+					foreach ($parts as $part) {
+						$children[] = new PartFile($this->node, $part);
+					}
+				}
+			}
 		}
 
 		return $children;

tpayen/server

apps/dav/lib/Upload/UploadFolder.php
@@ -24,7 +24,9 @@
  */
 namespace OCA\DAV\Upload;
 
+use OC\Files\ObjectStore\ObjectStoreStorage;
 use OCA\DAV\Connector\Sabre\Directory;
+use OCP\Files\ObjectStore\IObjectStoreMultiPartUpload;
 use OCP\Files\Storage\IStorage;
 use Sabre\DAV\Exception\Forbidden;
 use Sabre\DAV\ICollection;
@@ -68,6 +70,23 @@ class UploadFolder implements ICollection {
 
 		foreach ($tmpChildren as $child) {
 			$children[] = new UploadFile($child);
+		}
+
+		if ($this->storage->instanceOfStorage(ObjectStoreStorage::class)) {
+			/** @var ObjectStoreStorage $storage */
+			$objectStore = $this->storage->getObjectStore();
+			if ($objectStore instanceof IObjectStoreMultiPartUpload) {
+				$cache = \OC::$server->getMemCacheFactory()->createDistributed(ChunkingV2Plugin::CACHE_KEY);
+				$uploadSession = $cache->get($this->getName());
+				if ($uploadSession) {
+					$uploadId = $uploadSession[ChunkingV2Plugin::UPLOAD_ID];
+					$id = $uploadSession[ChunkingV2Plugin::UPLOAD_TARGET_FILE_ID];
+					$parts = $objectStore->getMultipartUploads($this->storage->getURN($id), $uploadId);
+					foreach ($parts as $part) {
+						$children[] = new PartFile($this->node, $part);
+					}
+				}
+			}
 		}
 
 		return $children;